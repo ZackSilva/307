9a5caaf84fd145da8119913c9b58e7315603bd57
ZackSilva <zsilva112501@gmail.com>
2022-02-14 18:25:20 -0500

    Added concept for new manipulatePartition() function using Duoquote's python-diskpart module.

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-# Imports psutil, the needed package for the assignment, platform, and sys
+# THIS SCRIPT IS MEANT FOR THE WINDOWS 10 OS
+# Imports psutil, the needed package for the assignment, and python-diskpart
 import psutil
-import platform
-import sys
+# from python-diskpart import diskpart
 
 # Shows the user all system partitions and their disk utilization
 def showDisks():
-    print(f"System partitions: {psutil.disk_partitions()}")
-    print("Disk utilization: " + psutil.disk_usage("c:\\")
+    print("--------------------------------------")
+    print("System partitions:")
+    print(psutil.disk_partitions())
+    print("Disk utilization in bytes: ")
+    print(psutil.disk_usage("/").total)
+    print("--------------------------------------")
+
+# Displays system memory statistics to the user
+def showMemory():
+    print(f"Total system memory installed : {psutil.virtual_memory().total} bytes")
+    print(f"Total system memory usage     : {psutil.virtual_memory().used} bytes")
+    print(f"Total system memory available : {psutil.virtual_memory().available} bytes")
+    print("--------------------------------------")
+
+# (pretends to) creates a new partition on the system and displays to user, then deletes it
+def manipulatePartition():
+    print("Creating new system partition:")
+    # diskpart.createPartition(5)
+    print(psutil.disk_partitions())
+    drive_letter = 'E'
+    print(f"New partition {drive_letter}: created.")
+    # diskpart.deletePartition(5)
+    print(f"partition {drive_letter}: deleted.")
+    print(psutil.disk_partitions())
 
 if __name__ == '__main__':
     showDisks()
+    showMemory()
+    manipulatePartition()
