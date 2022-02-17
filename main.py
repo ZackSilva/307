2edba47c512b8862ac6d967b21dde1d61d29bcf1
ZackSilva <zsilva112501@gmail.com>
2022-02-16 20:08:56 -0500

    Initial commit for networking branch

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
-# THIS SCRIPT IS MEANT FOR THE WINDOWS 10 OS
-# Imports psutil, the needed package for the assignment, and python-diskpart
-import psutil
-# from python-diskpart import diskpart
-
-# Shows the user all system partitions and their disk utilization
-def showDisks():
-    print("--------------------------------------")
-    print("System partitions:")
-    print(psutil.disk_partitions())
-    print("Disk utilization in bytes: ")
-    print(psutil.disk_usage("/").total)
-    print("--------------------------------------")
-
-# Displays system memory statistics to the user
-def showMemory():
-    print(f"Total system memory installed : {psutil.virtual_memory().total} bytes")
-    print(f"Total system memory usage     : {psutil.virtual_memory().used} bytes")
-    print(f"Total system memory available : {psutil.virtual_memory().available} bytes")
-    print("--------------------------------------")
-
-# (pretends to) creates a new partition on the system and displays to user, then deletes it
-def manipulatePartition():
-    print("Creating new system partition:")
-    # diskpart.createPartition(5)
-    print(psutil.disk_partitions())
-    drive_letter = 'E'
-    print(f"New partition {drive_letter}: created.")
-    # diskpart.deletePartition(5)
-    print(f"partition {drive_letter}: deleted.")
-    print(psutil.disk_partitions())
-
+# PyPort (utility name) displays the localhost's ports, IP address, specific port number,
+# and ONLY OPEN port statuses.
 if __name__ == '__main__':
-    showDisks()
-    showMemory()
-    manipulatePartition()
+    import socket
+    import sys
+    # Overarching function that defines localhost (and handles error if not found) for other functions
+    def PyPort():
+        try:
+            localhost = socket.gethostname()
+            print(f"localhost found. Name: {localhost}")
+        except socket.error as error:
+            print("localhost not found. Error code: " %error)
+        # Grabs the localhost's IP address (need to fine tune)
+        def get_local_addressing():
+            local_ip = (socket.getaddrinfo(localhost, 80))
+            print(f"Device LAN addressing: {local_ip}")
+        # Scans network for all OPEN ports and lists their IP addresses
+        def scan_ports():
+            print()
+
+        # Calls nested functions
+        get_local_addressing()
+        scan_ports()
+
+    # Calls main function to leverage utility
+    PyPort()
+
