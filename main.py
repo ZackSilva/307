a7cda16a11430b2f7b579b41b476175d1b1fc36b
ZackSilva <zsilva112501@gmail.com>
2022-02-24 23:06:40 -0500

    Initial commit for Assignment 5

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 # PySniffer (utility name) displays the current time and analyzes instances of
-# packets transmitted on/with common vulnerable ports and services (i.e. Telnet, FTP)
+# localhost ingress and egress packets transmitted on/with common vulnerable ports and services
+# (i.e. FTP 21, Telnet 23)
 if __name__ == '__main__':
-    import scapy
+    from scapy.all import *
     from datetime import datetime
     from datetime import date
     # Main function for utility
     def pySniffer():
-
         # Displays the current date and time in the console
         def getDateTime():
             print(f"Date: {date.today()}")
             now = datetime.now()
             timeNow = now.strftime('%H:%M:%S')
             print(f"Time: {timeNow}")
+            print("-" * 25)
+
+        # Displays to the user the destination IP, protocol, and basic
+        # contents of localhost packets
+        def getVulnPackets():
+
+            print()
+
         # Runs all nested functions
         getDateTime()
+        getVulnPackets()
 
     pySniffer()
 
