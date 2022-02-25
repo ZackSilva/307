b03aa25b4b6ce12be4e60729ee6532b99080329d
ZackSilva <zsilva112501@gmail.com>
2022-02-24 22:58:25 -0500

    Initial commit for Assignment 5

--- main.py
+++ main.py
@@ -1,39 +1,20 @@
-# PyPort (utility name) displays the localhost's open ports
+# PySniffer (utility name) displays the current time and analyzes instances of
+# packets transmitted on/with common vulnerable ports and services (i.e. Telnet, FTP)
 if __name__ == '__main__':
-    import socket
-    # Overarching function that defines localhost (and handles error if not found) for other functions
-    def PyPort():
-        print("Welcome to PyPort.")
-        print("~" * 40)
-        try:
-            localhost = socket.gethostname()
-            print(f"localhost found. Name: {localhost}")
-        except socket.error as error:
-            print("localhost not found. Error code: " %error)
-        # Grabs the localhost's IP address (need to fine tune)
-        def get_local_addressing():
-            local_ip = (socket.gethostbyname(localhost))
-            print(f"Device LAN addressing: {local_ip}")
-        # Scans network for all OPEN ports and lists their IP addresses
-        print("~" * 40)
-        def scan_ports():
-            print("Scanning all ports on device:")
-            try:
-                for port in range(1,65535):
-                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    socket.setdefaulttimeout(1)
-                    result = sock.connect_ex((localhost, port))
-                    if result == 0:
-                        print(f"Port {port}: OPEN".format(port))
-                    sock.close()
-            except KeyboardInterrupt:
-                print("Scanning stopped.")
-            except socket.gaierror:
-                print("Invalid hostname provided.")
-        # Calls nested functions
-        get_local_addressing()
-        scan_ports()
+    import scapy
+    from datetime import datetime
+    from datetime import date
+    # Main function for utility
+    def pySniffer():
 
-    # Calls main function to leverage utility
-    PyPort()
+        # Displays the current date and time in the console
+        def getDateTime():
+            print(f"Date: {date.today()}")
+            now = datetime.now()
+            timeNow = now.strftime('%H:%M:%S')
+            print(f"Time: {timeNow}")
+        # Runs all nested functions
+        getDateTime()
+
+    pySniffer()
 
