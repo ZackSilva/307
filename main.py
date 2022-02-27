3af278c16d549352c615af0737a1754a539e583b
ZackSilva <zsilva112501@gmail.com>
2022-02-27 14:46:03 -0500

    Initial commit for Assignment 5

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # PySniffer (utility name) displays the current time and analyzes instances of
-# localhost ingress and egress packets transmitted on/with common vulnerable ports and services
-# (i.e. FTP 21, Telnet 23)
+# localhost ingress and egress packets transmitted
 if __name__ == '__main__':
     from scapy.all import *
     from datetime import datetime
     from datetime import date
     # Main function for utility
     def pySniffer():
+        print("Welcome to PySniffer")
+        print("-" * 25)
         # Displays the current date and time in the console
         def getDateTime():
             print(f"Date: {date.today()}")
@@ -15,16 +16,17 @@ if __name__ == '__main__':
             print(f"Time: {timeNow}")
             print("-" * 25)
 
-        # Displays to the user the destination IP, protocol, and basic
-        # contents of localhost packets
-        def getVulnPackets():
-            print(sniff(filter='tcp', count=5))
-            print(sniff(prn=lambda x:x.summary(), count=5))
-            print()
+        # Displays to the user the source destination IP, protocol, and basic
+        # contents of localhost packets. The "count" variable instances are
+        # set to 100 by default to avoid flooding the console
+        def sniffPackets():
+            print("Sniffing 100 packets on localhost...")
+            print(sniff(filter='tcp', count=100))
+            print(sniff(prn=lambda x:x.summary(), count=100))
 
         # Runs all nested functions
         getDateTime()
-        getVulnPackets()
+        sniffPackets()
 
     pySniffer()
 
