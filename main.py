18c547eb49e111d86e6a3f9dd2f423fba9002376
ZackSilva <zsilva112501@gmail.com>
2022-04-19 17:39:51 -0400

    Initial commit for Assignment 7

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 # Imports and defines dependencies to leverage Firefox with Selenium capabilities
+import selenium
 from selenium import webdriver
+import requests
+import sys
+import webbrowser
+import bs4
 browser = webdriver.Firefox()
 type(browser)
+
+# Grabs commandline arguments
+def GetArgs():
+    print('Searching...')  # display text while downloading the search result page
+    res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q='+ ' '.join(sys.argv[1:]))
+    res.raise_for_status()
+
+# Function to implement PyLoader, the main function for this utility
+def PyLoader():
+    print("Welcome to PyLoader!")
+    print('~' * 40)
+    GetArgs()
+
 if __name__ == '__main__':
-    def PyScraper():
-        print("Welcome to PyScraper.")
-        print("*" * 30)
-
-        # This function prompts the user for a website URL in the format
-        # 'www.<URL>.<.com or other TLD>'
-        def GetWebsite():
-            website = input("Please enter a website URL using the format 'www.<URL>.<.com or other TLD>':")
-
-            # This function leverages Selenium to open the inputted URL via Firefox
-            def OpenBrowser():
-                browser.get('https://' + website)
-
-            OpenBrowser()
-
-        GetWebsite()
-
-    PyScraper()
+    PyLoader()
