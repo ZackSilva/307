271cbe3e19e5c50094c0900e28ca7493fcb6e32a
ZackSilva <zsilva112501@gmail.com>
2022-05-10 21:48:45 -0400

    Secondary commit for Assignment 8 - Ousted pygeoip package for geopy library

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+# After research it seems Nominatim does not allow the user to geolocate by IP so instead
+# I've demonstrated other geolocation elements
 # Imports the pygeoip package to leverage geolocation functions
-import pygeoip
-gi = pygeoip.GeoIP('GeoIP.dat')
-print("Welcome to PyLocator.")
-print('*' * 30)
+from geopy.geocoders import Nominatim
 
-# Function to ask the user for an IP address for analysis
-def getIP(address):
-    address = input("Please provide an IP address:")
+# Main function includes all code as it's really quite simple!
+def PyLocator():
+    print("Welcome to PyLocator.")
+    print('*' * 30)
+    loc = Nominatim(user_agent="GetLocationdetails")
+    getLoc = loc.geocode(input('What is your location? Please enter a city followed by state/region:'))
+    print('*' * 30)
 
-# Function to leverage the pygeoip package for specific geolocation
-def scanIP(address):
-
+    print(f"Address: {getLoc.address}")
+    print(f"Latitude: {getLoc.latitude}")
+    print(f"Longitude: {getLoc.longitude}")
 
 if __name__ == '__main__':
-    address = None
-    getIP(address)
-    scanIP(address)
-
+    PyLocator()
