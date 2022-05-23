d3d41a3130802009b666e6348394dfdfbaf1779e
ZackSilva <zsilva112501@gmail.com>
2022-05-23 19:28:19 -0400

    Fifth commit for Final Exam

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # This script validates SSL certificate status based on user input and outputs a log file.
-# The SSL and socket modules are imported to
+# The socket and ssl modules are imported to validate certificates of domains herein
 import socket
 import ssl
 
-# This function reads the domains.txt file provided in the assignment
+# This function reads the domains.txt file provided in the assignment and validates
+# Each domain's SSL certificate using a 'for' loop which reads each domain as a
+# Separate variable
 def PyVal():
+    print('Welcome to PyVal')
+    print('-' * 70)
     filepath = input("Please enter the full file path of the file's location:")
     domains_txt = open(filepath, 'r')
     print('Based on the file inputted, below are the domains to be evaluated:')
@@ -14,12 +18,11 @@ def PyVal():
     for line in lines:
         count += 1
         print("Line{}: {}".format(count, line))
-        domain = format(count, line())
+        domain = format(count, ".line")
         context = ssl.create_default_context()
         with socket.create_connection(domain, 443) as sock:
             with context.wrap_socket(sock, server_hostname=domain) as ssock:
                 print(ssock.version())
-
 
 if __name__ == '__main__':
     PyVal()
